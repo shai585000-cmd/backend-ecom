d1806f162e3c1e390037abf390db8cf60589ba14
offoange <andrepierre585@gmail.com>
2026-04-20 11:13:01 +0000

    feat: add images array to ProductSerializer returning all 4 product images

--- backend/produits/serializers.py
+++ backend/produits/serializers.py
@@ -22,7 +22,7 @@ class ProductSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Product
-        fields = ['id', 'image', 'user', 'categorie', 'categorie_id', 'promotion', 'promotion_price', 'name', 'description', 'price', 'is_featured', 'created_at', 'updated_at']
+        fields = ['id', 'image', 'images', 'user', 'categorie', 'categorie_id', 'promotion', 'promotion_price', 'name', 'description', 'price', 'is_featured', 'created_at', 'updated_at']
     
     def get_image(self, obj):
         """Retourne l'URL d'image correcte"""
@@ -55,3 +55,35 @@ class ProductSerializer(serializers.ModelSerializer):
         if request:
             return request.build_absolute_uri(f'/media/{image_str}')
         return image_str
+
+    def get_images(self, obj):
+        """Retourne toutes les images sous forme de tableau"""
+        images = []
+        image_fields = ['image', 'image_2', 'image_3', 'image_4']
+        
+        for idx, field in enumerate(image_fields):
+            img_field = getattr(obj, field, None)
+            if img_field:
+                img_str = str(img_field)
+                
+                # Même logique que get_image pour l'URL
+                if 'https%3A' in img_str or 'http%3A' in img_str:
+                    url = unquote(img_str)
+                    if url.startswith('/media/'):
+                        url = url[7:]
+                    images.append({'id': idx, 'image': url})
+                elif img_str.startswith('http://') or img_str.startswith('https://'):
+                    images.append({'id': idx, 'image': img_str})
+                else:
+                    try:
+                        url = img_field.url
+                        images.append({'id': idx, 'image': url})
+                    except Exception:
+                        request = self.context.get('request')
+                        if request:
+                            url = request.build_absolute_uri(f'/media/{img_str}')
+                            images.append({'id': idx, 'image': url})
+                        else:
+                            images.append({'id': idx, 'image': f'/media/{img_str}'})
+        
+        return images if images else None
